4f4a13c6cc66ea525684c4784accfa3e86bc3254
Nischal Timalsina <nischaltimalcna@outlook.com>
2024-05-15 00:04:10 +0545

    chore: update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,13 @@
 import { type SiteConfig } from "@/types"
 
 export const siteConfig: SiteConfig = {
-  name: "Boilerplate", // Generate a random 5-letter string
+  name: "Entail",
   description:
     "This is a placeholder description for the site. Replace it with your own content.",
-  url: "https://example.com", // Replace with a generic example URL
-  ogImage: "https://via.placeholder.com/1200x630.png", // Replace with a placeholder image
+  url: "https://entail.vercel.app", // Replace with a generic example URL
+  ogImage: "https://private-user-images.githubusercontent.com/94827555/330542095-79afaf78-5371-4a91-82e7-75aa1c3de5d2.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTU3MTA5OTMsIm5iZiI6MTcxNTcxMDY5MywicGF0aCI6Ii85NDgyNzU1NS8zMzA1NDIwOTUtNzlhZmFmNzgtNTM3MS00YTkxLTgyZTctNzVhYTFjM2RlNWQyLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA1MTQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNTE0VDE4MTgxM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTQ1NTc4M2YzOThiNGNlYWM0YzY2OTc1ZGMzNmE2MmJjOWVhMTFkNWY5OWJlMjc5M2FiNjMwNGNiNGJiNWFjNWEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.buHBRuRB-rnq6KHZemg6SlmhNACrmR_rTJ2UUgmKm_w", // Replace with a placeholder image
   links: {
-    twitter: "https://twitter.com/placeholder", // Replace with a placeholder Twitter handle
+    twitter: "https://twitter.com/entail", // Replace with a placeholder Twitter handle
     github: "https://github.com/nischaltimalsina/next-boilerplate.git",
   },
 }
